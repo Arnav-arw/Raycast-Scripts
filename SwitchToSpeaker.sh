d73cf43ab2df117752725ae23f6e6dbbefc288d3
Arnav Singhal <singhal.arnav@gmail.com>
2022-12-08 15:41:33 +0530

    Audio switching scripts

---
+++ SwitchToSpeaker.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Switch To AirPods
+# @raycast.mode silent
+# @raycast.author Arnav Singhal
+# @raycast.authorURL https://github.com/arnav-arw
+
+# @raycast.icon ▶️
+
+SwitchAudioSource -s "MacBook Air Speakers"
