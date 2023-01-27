bf1e94b2f9f29bb2b713b1a32c198f07448102fc
Arnav Singhal <singhal.arnav@gmail.com>
2023-01-27 16:58:47 +0530

    made better

--- New google meet.sh
+++ New google meet.sh
@@ -14,26 +14,18 @@ open https://meet.google.com/new
 MAX_TRIES=10
 TRIES=0
 
-get_url () {
-  URL=$(osascript -e 'tell application "Safari" to return URL of front document')
-}
-
 while true; do
   ((TRIES++))
 
-  # Allow some time between each iteration to perform an attempt
   sleep 1
 
-  # As we're doing `while true`, we do want to make sure we can
-  # exit the script when we feel we've tried too many times.
   if [[ "$TRIES" -gt "$MAX_TRIES" ]]; then
     echo "Could not copy Google Meet url"
     break;
   fi
 
-  get_url
+  URL=$(osascript -e 'tell application "Safari" to return URL of front document')
   
-  # Next, we want to make sure it's not still loading.
   if [[ $URL == *"new"* ]]; then
     continue
   fi
