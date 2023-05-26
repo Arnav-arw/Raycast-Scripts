7d0cfe80df34b6deeec0336d7a965723fe0931bb
Arnav Singhal <singhal.arnav@gmail.com>
2023-05-27 01:54:07 +0530

    new

---
+++ Terminal cmd.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Runs A Command
+# @raycast.mode fullOutput
+# @raycast.packageName Terminal
+# @raycast.author Arnav Singhal
+# @raycast.authorURL https://github.com/arnav-arw
+#
+# Optional parameters:
+# @raycast.icon 💻
+# @raycast.needsConfirmation false
+# @raycast.argument1 {"type": "text", "placeholder": "Command"}
+
+$1
