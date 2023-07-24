02586c00a067e7a02c440e783ba6d7bfa5af3d9c
Arnav Singhal <singhal.arnav@gmail.com>
2023-07-24 16:24:46 +0530

    minor change

--- New google meet.sh
+++ New google meet.sh
@@ -9,28 +9,4 @@
 # @raycast.authorURL https://github.com/arnav-arw
 # @raycast.description Start a Google Meet session
 
-open https://meet.google.com/new
-
-MAX_TRIES=10
-TRIES=0
-
-while true; do
-  ((TRIES++))
-
-  sleep 1
-
-  if [[ "$TRIES" -gt "$MAX_TRIES" ]]; then
-    echo "Could not copy Google Meet url"
-    break;
-  fi
-
-  URL=$(osascript -e 'tell application "Safari" to return URL of front document')
-  
-  if [[ $URL == *"new"* ]]; then
-    continue
-  fi
-
-  echo $URL | pbcopy
-  echo "Copied Google Meet url"
-  break
-done
+open -a /Applications/Arc.app "https://meet.google.com/new"
